46ed15f648ffee0ba0387bf599b3e1c6c556221b
Haseeb413 <haseebullah.11356@iqra.edu.pk>
2023-04-17 23:23:50 +0500

    CP-45
    CreateCareerNews API

--- spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/Entity/ContactUs.java
+++ spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/Entity/ContactUs.java
@@ -23,29 +23,26 @@ public class ContactUs {
     private Long id;
 
 
-    @NotNull
+
     @Column(name = "firstname")
     private String firstname;
 
-    @NotNull
+
     @Column(name = "lastname")
     private String lastname;
 
-    @NotNull
-    @Email
     @Column(name = "email")
     private String email;
 
 
-    @NotNull
     @Column(name = "phoneNumber")
     private String phonenumber;
 
-    @NotNull
+
     @Column(name = "message")
     private String message;
 
-    // Getters and setters, constructors, etc.
+
 
 
 }
---
+++ spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/service/CareerNewsService.java
@@ -0,0 +1,11 @@
+package com.bezkoder.spring.jpa.h2.service;
+
+import com.bezkoder.spring.jpa.h2.dto.CareerNewsDto;
+
+public interface CareerNewsService {
+
+
+    CareerNewsDto addNews(CareerNewsDto newsDTO);
+
+    }
+
