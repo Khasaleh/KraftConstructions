a9d7791cc2cd85cf1c589d71f06c9bbf0d551a96
jshah436 <emark1478@gmail.com>
2023-04-18 23:53:28 +0500

    routing done

--- spring-boot-angular-15-crud-example-master/angular-15-client/src/app/app-routing.module.ts
+++ spring-boot-angular-15-crud-example-master/angular-15-client/src/app/app-routing.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 const routes: Routes = [
-  { path: '', redirectTo: 'tutorials', pathMatch: 'full',
-}
-// {
-//   path: '/about-us', redirectTo :'about-us'
-// }
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full'},
+  {path: 'about-us', component: AboutUsComponent}
 ];
 
 @NgModule({
