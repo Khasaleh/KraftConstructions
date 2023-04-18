f6ca77e7fc66bd6b81ae41caf69c5482514d3354
jshah436 <emark1478@gmail.com>
2023-04-18 22:33:00 +0500

    about-us section is added

--- spring-boot-angular-15-crud-example-master/angular-15-client/src/app/app.module.ts
+++ spring-boot-angular-15-crud-example-master/angular-15-client/src/app/app.module.ts
@@ -5,16 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTutorialComponent,
-    TutorialDetailsComponent,
-    TutorialsListComponent
+    AboutUsComponent
+    
   ],
   imports: [
     BrowserModule,
