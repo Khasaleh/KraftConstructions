1c6063d7cb53b6bb56174d2c7dffb62847c33ce8
AwanUmair <umair.awan@cybersolution.technology>
2023-04-19 02:56:40 +0500

    CP-35 LoginApi, Jwt Token, CreateUserApi

--- spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/Entity/Review.java
+++ spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/Entity/Review.java
@@ -36,55 +36,6 @@ public class Review {
     @Enumerated(EnumType.STRING)
     private ApprovalStatus approvalStatus = ApprovalStatus.HIDDEN;
 
-
-//    public ApprovalStatus getApprovalStatus() {
-//        return approvalStatus;
-//    }
-//
-//    public void setApprovalStatus(ApprovalStatus approvalStatus) {
-//        this.approvalStatus = approvalStatus;
-//    }
-//
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getYourReview() {
-//        return YourReview;
-//    }
-//
-//    public void setYourReview(String yourReview) {
-//        YourReview = yourReview;
-//    }
-//
-//    public String getWorkExperience() {
-//        return WorkExperience;
-//    }
-//
-//    public void setWorkExperience(String workExperience) {
-//        WorkExperience = workExperience;
-//    }
-//
-//    public String getYourName() {
-//        return YourName;
-//    }
-//
-//    public void setYourName(String yourName) {
-//        YourName = yourName;
-//    }
-//
-//    public String getYourEmail() {
-//        return YourEmail;
-//    }
-//
-//    public void setYourEmail(String yourEmail) {
-//        YourEmail = yourEmail;
-//    }
 }
 
 
