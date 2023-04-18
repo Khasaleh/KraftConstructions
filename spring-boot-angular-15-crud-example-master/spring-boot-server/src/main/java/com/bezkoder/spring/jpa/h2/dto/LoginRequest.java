103a2c5cdffd59deeaf2d9708936d6a6db49d123
AwanUmair <umair.awan@cybersolution.technology>
2023-04-19 00:52:14 +0500

    CP-35 LoginApi, Jwt Token, CreateUserApi

--- spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/dto/LoginRequest.java
+++ spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/dto/LoginRequest.java
@@ -1,7 +1,12 @@
 package com.bezkoder.spring.jpa.h2.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotBlank;
 
+@Getter
+@Setter
 public class LoginRequest {
 	@NotBlank
   private String username;
@@ -9,19 +14,4 @@ public class LoginRequest {
 	@NotBlank
 	private String password;
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
