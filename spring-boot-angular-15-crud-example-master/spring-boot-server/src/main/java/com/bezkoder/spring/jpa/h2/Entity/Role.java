292453605551903e00372addf521eb8b55159dcd
AwanUmair <umair.awan@cybersolution.technology>
2023-04-20 22:40:03 +0500

    CP-35 added getUsersApi for Author

--- spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/Entity/Role.java
+++ spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/Entity/Role.java
@@ -1,7 +1,13 @@
 package com.bezkoder.spring.jpa.h2.Entity;
 
-import javax.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -13,27 +19,7 @@ public class Role {
     @Column(length = 20)
     private ERole name;
 
-    public Role() {
-
-    }
-
     public Role(ERole name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public ERole getName() {
-        return name;
-    }
-
-    public void setName(ERole name) {
         this.name = name;
     }
 }
