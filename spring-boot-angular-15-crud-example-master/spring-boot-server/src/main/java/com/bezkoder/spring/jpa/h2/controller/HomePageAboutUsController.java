2e6e3a480e2d3e7cb3c29bfea59593f9653ac3db
zainkhalid <mohammad.44896@iqra.edu.pk>
2023-05-04 20:09:26 +0500

    CP-68
    the videos are saved one time only and it can be updated

--- spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/controller/HomePageAboutUsController.java
+++ spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/controller/HomePageAboutUsController.java
@@ -10,7 +10,6 @@ import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,7 +18,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/homepageabout-us")
@@ -49,7 +47,7 @@ public class HomePageAboutUsController {
         return ResponseEntity.ok(homePageAboutUsResponseDTO);
     }
     @PostMapping("/upload-video")
-    @PreAuthorize("hasRole('" + Roles.ROLE_ADMIN + "')")
+   @PreAuthorize("hasRole('" + Roles.ROLE_ADMIN + "')")
     public ResponseEntity<Void> uploadVideo(@RequestParam("file") MultipartFile file) throws IOException {
         String videoUrl = saveVideoToFileSystem(file);
         homePageAboutUsService.updateAboutUsVideoUrl(ABOUT_US_ID, videoUrl);
@@ -57,9 +55,9 @@ public class HomePageAboutUsController {
     }
 
     private String saveVideoToFileSystem(MultipartFile file) throws IOException {
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        String fileName = "myvideo.mp4";
         String fileExtension = FilenameUtils.getExtension(fileName);
-        String storedFileName = UUID.randomUUID().toString() + "." + fileExtension;
+        String storedFileName =   "video.mp4" + "." + fileExtension;
         Path path = Paths.get("uploads/videos/" + storedFileName);
         Files.createDirectories(path.getParent());
         Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
