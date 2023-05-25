678c25e5132841bfc6a94cacb0b2fca12a1b5e7a
jshah436 <emark1478@gmail.com>
2023-05-26 02:08:41 +0500

    login

--- spring-boot-angular-15-crud-example-master/angular-15-client/src/app/components/login/login.component.ts
+++ spring-boot-angular-15-crud-example-master/angular-15-client/src/app/components/login/login.component.ts
@@ -11,80 +11,86 @@ import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
 
- selector: 'app-login',
- templateUrl: './login.component.html',
+    selector: 'app-login',
+    templateUrl: './login.component.html',
 
- styleUrls: ['./login.component.css'
+    styleUrls: ['./login.component.css'
 
-]
+    ]
 
 })
 
 export class LoginComponent {
 
-constructor(private builder: FormBuilder,private service: AuthService, private router:Router) {
+
+    constructor(private builder: FormBuilder, private service: AuthService, private router: Router) {
 
 
 
 
-}
+    }
 
 
 
 
-userdata:any;
+    userdata: any;
 
-responsedata:any;
-
-
-
-
-loginform=this.builder.group({
-
- username:this.builder.control(''),
-
- password:this.builder.control('')
-
-});
-
-ngOnInit():void {
-
-
-}
-
-
-proceedlogin() {
-
- if(this.loginform.valid) {
-
-this.service.proceedlogin(this.loginform.value).subscribe(result=> {
-
- if(result!=null) {
-
- this.responsedata = result;
-localStorage.setItem('token',this.responsedata.token);
-
- console.log("Navigate to admin page");
-
- this.router.navigate(['/admin']);
-
- // console.log("response",this.responsedata)
-
- }
+    responsedata: any;
 
 
 
 
+    loginform = this.builder.group({
 
-})
+        username: this.builder.control(''),
+
+        password: this.builder.control('')
+
+    });
+
+    ngOnInit(): void {
+
+
+    }
+
+    
+    proceedlogin() {
+
+        if (this.loginform.valid) {
+
+            this.service.proceedlogin(this.loginform.value).subscribe(result => {
+
+                if (result != null) {
+
+                    this.responsedata = result;
+                    localStorage.setItem('token', this.responsedata.token);
+
+                    console.log("Navigate to admin page");
+
+                    this.router.navigate(['/admin']);
+                   
+
+                    // console.log("response",this.responsedata)
+
+                }
+                
+               
 
 
 
 
 
+            })
 
-}
 
-}
+
+
+
+
+        }
+        
+        
+
+    }
 
 }
