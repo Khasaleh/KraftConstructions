3d79be351bc7a2849469f3e019e6c537dfa0fe0d
jshah436 <emark1478@gmail.com>
2023-06-05 18:57:31 +0500

    CRUD users

--- spring-boot-angular-15-crud-example-master/angular-15-client/src/app/components/about-us/about-us.component.ts
+++ spring-boot-angular-15-crud-example-master/angular-15-client/src/app/components/about-us/about-us.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { ImageService } from 'src/app/service/image.service';
+// import { ImageService } from 'src/app/service/image.service';
 import { UpdabtusService } from 'src/app/service/updabtus.service';
 
 @Component({
@@ -12,19 +12,21 @@ import { UpdabtusService } from 'src/app/service/updabtus.service';
 
 export class AboutUsComponent {
  
-  constructor(private aboutusdata : UpdabtusService, private builder: FormBuilder, public imageService: ImageService ) {}
+  constructor(private aboutusdata : UpdabtusService, private builder: FormBuilder) {}
   
-  userdata : any;
+  userdata! : any;
   desc = "Kraft Construction is a family-owned business that was founded in Cincinnati, Ohio in 1971. Over the years we have evolved into a residential remodeling company dedicated to helping homeowners create their dream spaces with our individualized design approach and our dedicated employees working in your homes that have a combined 106 years of experience."
   abc = "We strive to make our customers happy with every project we do. Whether it's adding an outdoor space for family and friends to gather, a new addition for a growing family, or updating your kitchen or bathroom.\nWe will work with you from beginning to end to have your dream home."
   
 
 
-  showdata = this.builder.group({
+    showdata = this.builder.group({
 
     title: this.builder.control(''),
 
-    description: this.builder.control('')
+    description: this.builder.control(''),
+
+    // imageUrl: this.builder.control('')
 
 });
   
@@ -33,6 +35,7 @@ export class AboutUsComponent {
       console.log(result);
       this.userdata=result;
     });
+   
     
   }
   
--- spring-boot-angular-15-crud-example-master/angular-15-client/src/app/components/showusers/showusers.component.ts
+++ spring-boot-angular-15-crud-example-master/angular-15-client/src/app/components/showusers/showusers.component.ts
@@ -1,9 +1,103 @@
 import { Component } from '@angular/core';
-
+import { AddUserService } from 'src/app/service/add-user.service';
+import { AddUser } from 'src/app/model/add-user';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import {User} from 'src/app/model/user'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-showusers',
   templateUrl: './showusers.component.html',
   styleUrls: ['./showusers.component.css']
 })
 export class ShowusersComponent {
+  userdata!:any[];
+  userDetail!:FormGroup;
+  userObj : AddUser = new AddUser();
+  oldUser!: string;
+  
+  constructor(private adduserdata : AddUserService, private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getAllUsers();
+
+    this.userDetail = this.formBuilder.group ({ 
+      username : [''],
+      email : [''],
+      firstname : [''],
+      lastname : [''],
+      password : [''],
+      role: ['']
+    });
+  
+    
+  }
+  getAllUsers() {
+  this.adduserdata.getAllUsers().subscribe((result)=>{
+    console.log(result);
+    this.userdata=result;
+   
+  });
 }
+deleteUser(username: string) {
+  this.adduserdata.deleteUser(username).subscribe(
+    () => {
+      this.userdata = this.userdata.filter(u => u.username !== username);
+      this.getAllUsers();
+    },
+    err => {
+      console.log(err);
+    
+    }
+  );
+}
+
+
+// updateUser(user: AddUser) {
+//   this.adduserdata.updateUser(this.userObj,this.oldUser).subscribe(res=> {
+//     console.log(this.userObj.username,this.userObj.email,this.userObj.roles);
+//     console.log(res);
+//     this.getAllUsers();
+//   },err=> {
+//     console.log(err);
+//   });
+// }
+editUser(user: User) {
+  this.oldUser = user.username;
+  this.userDetail.patchValue({
+    username: user.username,
+    email: user.email,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    password: '',
+    role: user.roles && user.roles.length > 0 ? user.roles[0].name : null
+  });
+}
+  updateUser() {
+    const updatedUser: AddUser = {
+    username: this.userDetail.value.username,
+    email : this.userDetail.value.email,
+    firstname : this.userDetail.value.firstname,
+    lastname : this.userDetail.value.lastname,
+    password : this.userDetail.value.password,
+    role: this.userDetail.value.role
+
+  };
+    console.log(updatedUser);
+
+  
+    this.adduserdata.updateUser(updatedUser,this.oldUser).subscribe((result)=>{
+      console.log(result);
+   
+      this.getAllUsers();
+    },err=>{
+      console.log(err);
+    });
+  
+
+  }
+
+ 
+  
+}
+  
+
