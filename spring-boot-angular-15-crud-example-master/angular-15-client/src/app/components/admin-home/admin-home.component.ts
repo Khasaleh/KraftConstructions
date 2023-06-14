db952085a7373aa5153219aac65efc9c8cfc6c49
ArsalanAhmad2000 <129388410+ArsalanAhmad2000@users.noreply.github.com>
2023-06-15 03:48:21 +0500

    career news toggle functionality work for the career header element

--- spring-boot-angular-15-crud-example-master/angular-15-client/src/app/components/admin-home/admin-home.component.ts
+++ spring-boot-angular-15-crud-example-master/angular-15-client/src/app/components/admin-home/admin-home.component.ts
@@ -13,33 +13,25 @@ interface Image {
   templateUrl: './admin-home.component.html',
   styleUrls: ['./admin-home.component.css', '../../../styles.css']
 })
-
 export class AdminHomeComponent {
   selectedOption: string = 'interior remodeling';
   options: string[] = ['interior remodeling', 'New Additions'];
   title = 'angular-template-ckeditor5-classic';
   public Editor = ClassicEditor;
-
   public onReady(editor: any) {
     console.log("CKEditor5 Angular Component is ready to use!", editor);
   }
   public onChange({ editor }: ChangeEvent) {
   }
-
   videoLink: any;
   fileURL!: File;
-
-
   onFileSelected(event: any) {
     this.fileURL = event.target.files[0];
     this.videoLink = URL.createObjectURL(this.fileURL);
-
     this.onVideoUploadBtnClick();
     console.log(this.fileURL)
     console.log(this.videoLink, "video link");
   }
-
-
   myForm = new FormGroup({
     name: new FormControl(''),
     email: new FormControl(''),
@@ -53,34 +45,26 @@ export class AdminHomeComponent {
   deleteForm(index: number) {
     this.forms.splice(index, 1);
   }
-  //for Api integration
-
   aboutUsForm!: FormGroup;
   bannerForm!: FormGroup;
-  linkStatus: boolean =  false;
+  linkStatus: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private homeService: HomeServiceService,
   ) { }
-
   onVideoUploadBtnClick() {
-
     const formData = new FormData();
     formData.append('file', this.fileURL);
-
     this.homeService.saveVideo(formData).subscribe(
       response => {
-        // Handle the API response here
         console.log(response);
       },
       error => {
-        // Handle any error that occurs during the API request
         console.error(error);
       }
     );
   }
-
+  apiData: any;
   ngOnInit(): void {
-
     this.aboutUsForm = this.formBuilder.group({
 
       textEditor: ['', Validators.required],
@@ -93,8 +77,12 @@ export class AdminHomeComponent {
       bannerDescription: ['', Validators.required],
       linkStatus: [this.linkStatus]
     });
-    
-
+    this.homeService.getHomePageData().subscribe(
+      data => {
+        this.apiData = data;
+        console.log("ResponseData1", data);
+      },
+    )
   }
   onSubmit1() {
 
@@ -104,19 +92,16 @@ export class AdminHomeComponent {
       const linkStatus = this.bannerForm.get('linkStatus')?.value;
       this.homeService.saveBannerData(bannerLink, bannerDescription, linkStatus).subscribe(
         response => {
-          // Handle the API response here
           console.log(" banner data submit successfully");
           console.log("response", response)
         },
         error => {
-          // Handle any error that occurs during the API request
           console.error(error);
         }
       )
     }
     this.uploadImages();
   }
-
   onSubmit() {
 
     if (this.aboutUsForm.valid) {
@@ -124,19 +109,15 @@ export class AdminHomeComponent {
       const addLink = this.aboutUsForm.get('addLink')?.value;
       this.homeService.saveHomepageData(textEditor, addLink).subscribe(
         response => {
-          // Handle the API response here
           console.log("data submit successfully");
           console.log("response", response)
         },
         error => {
-          // Handle any error that occurs during the API request
           console.error(error);
         }
       )
     }
-
   }
-  
   isCardBodyVisible: boolean = false;
   toggleCardBody() {
     this.isCardBodyVisible = !this.isCardBodyVisible;
@@ -145,24 +126,9 @@ export class AdminHomeComponent {
   toggleLink() {
     this.isLinkVisible = !this.isLinkVisible;
   }
-  // images: Image[] = [];
   images: File[] = [];
-  // imageLink: any;
   imageLink: any[] = [];
   activeIndex = 0;
-
-  // onFileSelected1(event: any) {
-  //   const files = event.target.files;
-  //   for (let i = 0; i < files.length; i++) {
-  //     const file = files[i];
-  //     const reader = new FileReader();
-  //     reader.onload = (e) => {
-  //       const imageUrl = reader.result as string;
-  //       this.images.push({ url: imageUrl });
-  //     };
-  //     reader.readAsDataURL(file);
-  //   }
-  // }
   onFileSelected1(event: any) {
     const files = event.target.files;
     for (let i = 0; i < files.length; i++) {
@@ -170,42 +136,27 @@ export class AdminHomeComponent {
       this.images.push(file);
       this.imageLink.push(URL.createObjectURL(file));
     }
-    // for (let i = 0; i < this.images.length; i++) {
-    //   this.imageLink = URL.createObjectURL(this.images[i]);
-    // }
-    console.log(this.imageLink, "image link");
   }
-
   uploadImages1() {
     const formData = new FormData();
     for (let i = 0; i < this.images.length; i++) {
       formData.append('images', this.images[i]);
     }
-
-    // Perform upload logic here, e.g., send formData to server
     console.log(formData);
     this.homeService.saveBannnerImage(formData).subscribe(
       response => {
-        // Handle the API response here
         console.log(response);
       },
       error => {
-        // Handle any error that occurs during the API request
         console.error(error);
       }
     );
   }
-
   uploadImages() {
     this.uploadImages1();
     console.log(this.images);
   }
-  // getPreviewURL(image: File): string {
-  //   return URL.createObjectURL(image);
-  //   // this.cdr.detectChanges();
-  // }
   setActiveIndex(index: number) {
     this.activeIndex = index;
   }
-
 }
