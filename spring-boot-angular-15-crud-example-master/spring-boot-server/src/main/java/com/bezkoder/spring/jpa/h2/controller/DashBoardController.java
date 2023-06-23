32dcadca4be3d290ad004aab84dfbd50b1eaf5c9
AwanUmair <umair.awan@cybersolution.technology>
2023-06-23 19:07:23 +0500

    added authorization

--- spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/controller/DashBoardController.java
+++ spring-boot-angular-15-crud-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/controller/DashBoardController.java
@@ -2,7 +2,9 @@ package com.bezkoder.spring.jpa.h2.controller;
 
 import com.bezkoder.spring.jpa.h2.dto.DashBoardStatistics;
 import com.bezkoder.spring.jpa.h2.service.DashBoardService;
+import com.bezkoder.spring.jpa.h2.util.Roles;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ public class DashBoardController {
     private DashBoardService dashBoardService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('" + Roles.ROLE_ADMIN + "','" + Roles.ROLE_PHOTOGRAPHER + "')")
     public DashBoardStatistics getHomepageStatistics() {
         return dashBoardService.getHomepageStatistics();
     }
